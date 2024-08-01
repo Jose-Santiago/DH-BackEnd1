4842a50fdd2a3a06fd54652e948051781fcdec00
José Santiago <santiagojos@hotmail.com>
2024-07-31 19:07:11 -0600

    Clase 07, log4J mesa de trabajo agregando mas elementos a lista

--- Clase 7- Logger y Debug/C7ListaPromedioLogger/src/presencial/Cliente.java
+++ Clase 7- Logger y Debug/C7ListaPromedioLogger/src/presencial/Cliente.java
@@ -5,10 +5,16 @@ public class Cliente {
         App listado= new App();
 
         listado.calcularPromedio();
+
         for (int i=0; i<6;i++){
             listado.agregarEnterosAlaLista(i);
         }
 
         listado.calcularPromedio();
+
+        for (int i=6; i<12;i++){
+            listado.agregarEnterosAlaLista(i);
+        }
+
     }
 }
